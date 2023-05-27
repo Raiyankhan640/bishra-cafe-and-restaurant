fb75e934c525cb3f2bd7a3f6d8c16b181ee712b2
Raiyankhan640 <raiyankhan640@gmail.com>
2023-05-27 23:49:10 +0600

    starts from here

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
